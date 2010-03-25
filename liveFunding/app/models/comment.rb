11c3d63bbfe6a0cd43a36c5555bca155915b32ef
unknown <ZK@.(none)>
2010-03-25 10:06:52 +0200

    Add comment in model_comment, delete the third part in the homepage

--- liveFunding/app/models/comment.rb
+++ liveFunding/app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ActiveRecord::Base
-  validates_presence_of :content, :commenter
-  validates_length_of :commenter, :in => 2..15
+  validates_presence_of :content, :commenter	# Content and commenter can't be empty
+  validates_length_of :commenter, :in => 2..15	# Minimum content is 2 characters,and max is 15
   validates_length_of :content, :in => 2..2000
-  belongs_to :user
-  belongs_to :transaction
-end
+  belongs_to :transaction						# One transaction has many comments			
+  belongs_to :user								
+ end
