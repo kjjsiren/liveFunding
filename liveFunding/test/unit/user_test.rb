4a76b381d4dbfb6d67fa3268f9469adc5291094a
qi <qiyn_19850222@hotmail.com>
2010-05-06 14:00:26 +0300

    unit test for user

--- liveFunding/test/unit/user_test.rb
+++ liveFunding/test/unit/user_test.rb
@@ -1,8 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "invalid with empty attributes" do
+    user = User.new
+    assert !user.valid?
+    assert user.errors.invalid?(:user_username)
   end
 end
